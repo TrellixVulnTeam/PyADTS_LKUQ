f27844709858ccefddde386b04fa6459bb0a6035
Larry Shaw <larryshaw0079@live.com>
2020-08-12 17:05:36 +0800

    update metrics

--- pyadt/evaluation/metrics.py
+++ pyadt/evaluation/metrics.py
@@ -1,98 +1,85 @@
 import numpy as np
 
 from sklearn.metrics import precision_score, recall_score, f1_score
+from sklearn.metrics import auc, roc_auc_score, precision_recall_curve
+from sklearn.utils import deprecated
 
 
-def reconstruct_predict(y_pred, y_true, delay=7):
-    """
-
-    """
-    change_points = np.where(y_true[1:] != y_true[:-1])[0] + 1
-    is_anomaly = (y_true[0] == 1)
-    modified_y_pred = np.array(y_pred)
-    current_pos = 0
-
-    for point in change_points:
+def __reconstruct_prediction(score: np.ndarray, label: np.ndarray, delay=None, inplace=False) -> np.ndarray:
+    assert np.shape(score) == np.shape(label)
+    if delay is None:
+        delay = len(score)
+    splits = np.where(label[1:] != label[:-1])[0] + 1
+    is_anomaly = label[0] == 1
+    new_array = np.copy(score) if not inplace else score
+    pos = 0
+    for sp in splits:
         if is_anomaly:
-            if 1 in y_pred[current_pos: min(current_pos + delay + 1, point)]:
-                modified_y_pred[current_pos:point] = 1
-            else:
-                modified_y_pred[current_pos:point] = 0
-
+            ptr = min(pos + delay + 1, sp)
+            new_array[pos: ptr] = np.max(new_array[pos: ptr])
+            new_array[ptr: sp] = np.maximum(new_array[ptr: sp], new_array[pos])
         is_anomaly = not is_anomaly
-        current_pos = point
-
-    point = len(y_true)
-
+        pos = sp
+    sp = len(label)
     if is_anomaly:
-        if 1 in y_pred[current_pos: min(current_pos + delay + 1, point)]:
-            modified_y_pred[current_pos:point] = 1
-        else:
-            modified_y_pred[current_pos:point] = 0
-
-    return modified_y_pred
+        ptr = min(pos + delay + 1, sp)
+        new_array[pos: sp] = np.max(new_array[pos: ptr])
+    return new_array
 
 
-def reconstruct_label(timestamp, label):
-    """
-
-    """
-    timestamp = np.asarray(timestamp, np.int64)
-    index = np.argsort(timestamp)
-
-    timestamp_sorted = np.asarray(timestamp[index])
-    interval = np.min(np.diff(timestamp_sorted))
-
-    label = np.asarray(label, np.int64)
-    label = np.asarray(label[index])
-
-    idx = (timestamp_sorted - timestamp_sorted[0]) // interval
-
-    label_reconstructed = np.zeros(shape=((timestamp_sorted[-1] - timestamp_sorted[0]) // interval + 1,), dtype=np.int)
-    label_reconstructed[idx] = label
-
-    return label_reconstructed
+def __ignore_missing(*args, missing):
+    result = []
+    for arr in args:
+        _arr = np.copy(arr)
+        result.append(_arr[missing != 1])
+    return tuple(result)
 
 
-def get_modified_predict_label(y_preds, y_trues, delay=7):
-    """
-    Compute modified labels
-    Args:
-        y_preds: shape (num_sample, num_series)
-        y_trues: shape (num_sample, num_series)
-        timestamp: 
-        delay: 
-    """
-    assert (type(y_preds) == type(y_trues))
-    if type(y_preds) is not list:
-        y_pred = reconstruct_predict(y_preds, y_trues, delay)
-        y_true = y_trues
-    elif len(y_preds) == 1:
-        y_pred = reconstruct_predict(y_preds[0], y_trues[0], delay)
-        y_true = y_trues[0]
-    else:
-        y_pred = []
-        for i, data in enumerate(y_preds):
-            y_pred.append(reconstruct_predict(y_preds[i], y_trues[i], delay))
-        y_pred = np.concatenate(y_pred)
-        y_true = np.concatenate(y_trues)
+@deprecated('Using precision to evaluate anomaly detection algorithms is not recommended.')
+def best_precision(score: np.ndarray, label: np.ndarray, delay: int=None):
+    if delay is not None:
+        score = __reconstruct_prediction(score, label, delay=delay, inplace=False)
 
-    return y_pred, y_true
+    ps, rs, ts = precision_recall_curve(label, score)
+    fs = 2 * ps * rs / np.clip(ps + rs, a_min=1e-8, a_max=None)
+
+    return ps[np.argmax(fs)]
 
 
-def modified_precision(y_pred, y_true, delay=7):
-    y_pred, y_true = get_modified_predict_label(y_pred, y_true, delay)
+@deprecated('Using recall to evaluate anomaly detection algorithms is not recommended.')
+def best_recall(score: np.ndarray, label: np.ndarray, delay: int=None):
+    if delay is not None:
+        score = __reconstruct_prediction(score, label, delay=delay, inplace=False)
 
-    return precision_score(y_true, y_pred)
+    ps, rs, ts = precision_recall_curve(label, score)
+    fs = 2 * ps * rs / np.clip(ps + rs, a_min=1e-8, a_max=None)
+
+    return rs[np.argmax(fs)]
 
 
-def modified_recall(y_pred, y_true, timestamp, delay=7):
-    y_pred, y_true = get_modified_predict_label(y_pred, y_true, delay)
+@deprecated('Using F1-score to evaluate anomaly detection algorithms is not recommended.')
+def best_f1(score: np.ndarray, label: np.ndarray, delay: int=None):
+    if delay is not None:
+        score = __reconstruct_prediction(score, label, delay=delay, inplace=False)
 
-    return recall_score(y_true, y_pred)
+    ps, rs, ts = precision_recall_curve(label, score)
+    fs = 2 * ps * rs / np.clip(ps + rs, a_min=1e-8, a_max=None)
+
+    return np.max(fs[np.isfinite((fs))])
 
 
-def modified_f1(y_pred, y_true, timestamp, delay=7):
-    y_pred, y_true = get_modified_predict_label(y_pred, y_true, delay)
+def roc_auc(score: np.ndarray, label: np.ndarray, delay: int=None):
+    if delay is not None:
+        score = __reconstruct_prediction(score, label, delay=delay, inplace=False)
 
-    return f1_score(y_true, y_pred)
+    return roc_auc_score(label, score)
+
+
+def pr_auc(score: np.ndarray, label: np.ndarray, delay: int=None):
+    if delay is not None:
+        score = __reconstruct_prediction(score, label, delay=delay, inplace=False)
+
+    ps, rs, _ = precision_recall_curve(label, score)
+    ids = np.argsort(rs)
+
+    return auc(rs[ids], ps[ids])
