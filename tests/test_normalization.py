453cf58c1b836365ed05746a4984a482770b193e
larryshaw0079 <larryshaw0079@live.com>
2020-08-21 11:24:31 +0800

    add normalization test

--- tests/test_normalization.py
+++ tests/test_normalization.py
@@ -1,7 +1,13 @@
 import sys
 
+from pyadts.data.repository import get_kpi
+from pyadts.data.preprocessing import series_normalize
+
 sys.path.append('..')
 
 
 def test_series_normalize():
-    assert False
+    data_df, meta_df = get_kpi('./data/kpi', kpi_id=0)
+    series_normalize(data_df, method='minmax')
+    series_normalize(data_df, method='negpos1')
+    series_normalize(data_df, method='zscore')
